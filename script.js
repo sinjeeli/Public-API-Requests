d66cabf03d60ef746a4e7a20f73b55d18b8ae96f
sinjeeli <sinjeeli@gmail.com>
2023-06-25 22:02:12 -0400

    updates

--- script.js
+++ script.js
@@ -5,27 +5,71 @@ function showImage(display) {
 
     let box;
 
-display.forEach(display)=> {
+display.forEach((display) => {
     box = document.createElement('div');
     box.className = 'card';
-    box.innerHTML = `
-    
+    box.innerHTML = 
+    `
     <div class="card-img-container">
-    <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+    <img class="card-img" src=${display.picture.medium}" alt=${display.name.last}>
 </div>
 <div class="card-info-container">
-    <h3 id="name" class="card-name cap">first last</h3>
-    <p class="card-text">email</p>
-    <p class="card-text cap">city, state</p>
+    <h3 id="name" class="card-name cap">${display.name.first} ${display.name.last}</h3>
+    <p class="card-text">${display.email}</p>
+    <p class="card-text cap">${display.location.city}, ${display.location.state}</p>
 </div>
-
     `
 
+    gallery.insertAdjacentElement('beforeend', box);
+
+    box.addEventListener('click', (e)=>{
+
+        modalFunction(display)
+    });
+
+
+});
+
+};
+
+function modalFunction(display){
+
+
+
+    let modalDisplay = document.createElement('div');
+    modalDisplay.className = 'modal-container';
+
+    modalDisplay.innerHTML = 
+    `
+    <div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+    <img class="modal-img" src="${display?.picture?.medium}" alt="${display?.name?.last}">
+    <h3 id="name" class="modal-name cap">${display?.name?.first} ${display?.name?.last}</h3>
+    <p class="modal-text">${display?.email}</p>
+    <p class="modal-text cap">${display?.location?.city}</p>
+    <hr>
+    <p class="modal-text">${display?.phone}</p>
+    <p class="modal-text">${display?.location?.street?.number}, ${card?.location?.street?.name}, ${card?.location?.city}, ${card?.location?.state} ${card?.location?.postcode}</p>
+    <p class="modal-text">Birthday: ${month}${day}${year}</p>
+    </div>
+    `
+    
+//
+    gallery.appendChild(modalDisplay);
+
+    //
+
+modalDisplay.addEventListener('click', (e)=>{
+    if(e.target.tagName === 'STRONG'){
+        gallery.removeChild(gallery.lastElementChild);
+
+        }
+
+    });
+
 
 }
-
-}
-
 
 
 
