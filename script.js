8c8080e2395952176709b038c691e2f634343669
sinjeeli <sinjeeli@gmail.com>
2023-06-25 22:44:15 -0400

    adding notes and project complete

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const gallery = document.getElementById('gallery');
-//
+//declaring the gallery variable for future markups
 
 //
 fetch("https://randomuser.me/api/?results=12")
 .then((response)=> response.json())
 .then((data)=> showImage(data.results))
+//fetching required data from randomuser api, with 12 results
 
 function showImage(display) {
 
     let box;
-
+//forEach will iterate for every card being displayed
 display.forEach((display, index) => {
     box = document.createElement('div');
+    //create a div for each card
     box.className = 'card';
+    //applying css
     box.innerHTML = `
     <div class="card-img-container">
     <img class="card-img" src="${display.picture.medium}" alt="${display.name.last}">
@@ -22,11 +25,14 @@ display.forEach((display, index) => {
     <p class="card-text">${display.email}</p>
     <p class="card-text cap">${display.location.city}, ${display.location.state}</p>
 </div>`
+//gallery markup from index.html sample
 
     gallery.insertAdjacentElement('beforeend', box);
+    //required statement per instructions for displaying
 
     box.addEventListener('click', (e)=>{
-
+        //event listener for clicks on the card, will show
+        //the modal
         modalFunction(display, index)
     })
 
@@ -36,26 +42,30 @@ display.forEach((display, index) => {
 }
 
 function modalFunction(display, index){
-
-
+//function to display the modal
 
     let modalDisplay = document.createElement('div');
+    //create the div to display the modal
     modalDisplay.className = 'modal-container';
+    //apply css
 
 
     
 //
     gallery.appendChild(modalDisplay);
+    //append to gallery
 //
 
 const dob = new Date(display.dob.date);
+//changing the dob to only show year month date, and not time
 const formattedDate = dob.toLocaleDateString('en-US', {
-
     year: 'numeric',
     month: 'long',
     day: 'numeric'
 
 });
+//
+
 
 
     modalDisplay.innerHTML = `
@@ -73,7 +83,7 @@ const formattedDate = dob.toLocaleDateString('en-US', {
     </div>
     </div>
     `
-    //
+    //modal markup from html sample
 
 modalDisplay.addEventListener('click', (e)=> {
     if(e.target.tagName === 'STRONG'){
@@ -82,6 +92,8 @@ modalDisplay.addEventListener('click', (e)=> {
         }
 
     })
+    //event listener for the click for the X-tagName is STRONG;
+
 
 
 }
